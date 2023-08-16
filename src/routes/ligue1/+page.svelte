529f2e1a7ae6f7bbcba0b69095d6d9e5a9e8031f
Sébastien <sgortais@net-com.fr>
2023-08-16 09:52:42 +0200

    Estimation de points

--- src/routes/ligue1/+page.svelte
+++ src/routes/ligue1/+page.svelte
@@ -1,27 +1,27 @@
 <script>
-    import { onMount } from 'svelte';
+    import {onMount} from 'svelte';
     import axios from 'axios';
-    import { load } from 'cheerio';
-    
+    import {load} from 'cheerio';
+
     const nbMaxJ = 34;
     let ptsRelegation = 0;
     let ptsEurope = 0;
     let ptsLDC = 0;
     let ptsPremier = 0;
-    
+
     const url = 'https://www.ligue1.fr/classement';
-    
+
     /**
-    * @type {any[]}
-    */
+     * @type {any[]}
+     */
     let clubs = [];
-    
+
     onMount(async function () {
         const response = await axios.get(url)
         const html = response.data;
         const $ = load(html);
-        
-        $('.GeneralStats-item--club .desktop-item').each(function() {
+
+        $('.GeneralStats-item--club .desktop-item').each(function () {
             const name = $(this).text().trim();
             const line = {
                 name: name,
@@ -30,17 +30,16 @@
                 diff: $(this).parent().next().next().next().next().next().next().next().next().text(),
                 maxPts: 0
             };
-            line.maxPts = parseInt(line.pts) + ( 3 * (nbMaxJ - parseInt(line.joues)))
-            clubs = [...clubs, line];            
+            line.maxPts = parseInt(line.pts) + (3 * (nbMaxJ - parseInt(line.joues)))
+            clubs = [...clubs, line];
         });
-        ptsRelegation = parseInt(clubs[16].pts) + ( 3 * (nbMaxJ - parseInt(clubs[16].joues)));
-        ptsEurope = parseInt(clubs[5].pts) + ( 3 * (nbMaxJ - parseInt(clubs[5].joues)));
-        ptsLDC = parseInt(clubs[4].pts) + ( 3 * (nbMaxJ - parseInt(clubs[4].joues)));
-        ptsPremier = parseInt(clubs[1].pts) + ( 3 * (nbMaxJ - parseInt(clubs[1].joues)));
+        ptsRelegation = parseInt(clubs[16].pts) + (3 * (nbMaxJ - parseInt(clubs[16].joues)));
+        ptsEurope = parseInt(clubs[5].pts) + (3 * (nbMaxJ - parseInt(clubs[5].joues)));
+        ptsLDC = parseInt(clubs[4].pts) + (3 * (nbMaxJ - parseInt(clubs[4].joues)));
+        ptsPremier = parseInt(clubs[1].pts) + (3 * (nbMaxJ - parseInt(clubs[1].joues)));
     });
-    
-    
-    
+
+
 </script>
 
 <div class="textcenter">
@@ -48,17 +47,18 @@
 </div>
 <table cellpadding="13" cellspacing="0">
     <thead>
-        <tr>
-            <td>#</td>
-            <td>Name</td>
-            <td>Pts</td>
-            <td>GA</td>
-            <td>J</td>
-            <td>Max</td>
-        </tr>
+    <tr>
+        <td>#</td>
+        <td>Name</td>
+        <td>Pts</td>
+        <td>GA</td>
+        <td>J</td>
+        <td>Max</td>
+        <td>Est.</td>
+    </tr>
     </thead>
     <tbody>
-        {#each clubs as club, index}
+    {#each clubs as club, index}
         <tr class='
         {club.pts <= clubs[16].maxPts ? "releguable " : ""} 
         {club.maxPts < clubs[15].pts ? "relegue " : ""} 
@@ -67,63 +67,75 @@
         {club.pts > ptsLDC ? "ldc " : ""}
         {club.maxPts >= clubs[2].pts ? "ldcable " : ""} 
         '>
-        
-        <td>
-            {index + 1 }
-        </td>
-        <td>
-            {club.name}
-        </td>
-        <td>
-            {club.pts}
-        </td>
-        <td>{club.diff}</td>
-        <td>{club.joues}</td>
-        <td>{club.maxPts}</td>
-    </tr> 
+
+            <td>
+                {index + 1 }
+            </td>
+            <td>
+                {club.name}
+            </td>
+            <td>
+                {club.pts}
+            </td>
+            <td>{club.diff}</td>
+            <td>{club.joues}</td>
+            <td>{club.maxPts}</td>
+            <td>{Math.round(club.pts + ((club.pts / club.joues) * (nbMaxJ - club.joues )))}</td>
+        </tr>
     {/each}
-</tbody>
+    </tbody>
 </table>
 
 
 <style>
-    .textcenter{
+    .textcenter {
         text-align: center;
     }
-    thead td{
+
+    thead td {
         font-weight: bold;
     }
-    table{
+
+    table {
         width: 100%;
         height: 100vh;
     }
-    tbody tr:nth-child(odd){
+
+    tbody tr:nth-child(odd) {
         background: #eee;
     }
+
     tbody tr:nth-last-child(3),
     tbody tr:nth-last-child(2),
-    tbody tr:nth-last-child(1){
+    tbody tr:nth-last-child(1) {
         background-color: rgb(255, 187, 187);
     }
-    table tbody tr.releguable td:first-child{
-        background-color: rgba(255,0,0,0.2);
+
+    table tbody tr.releguable td:first-child {
+        background-color: rgba(255, 0, 0, 0.2);
     }
-    table tbody tr.relegue td:first-child{
-        background-color: red; 
+
+    table tbody tr.relegue td:first-child {
+        background-color: red;
     }
-    table tbody tr.europable td:first-child{
+
+    table tbody tr.europable td:first-child {
         background-color: rgba(25, 88, 0, 0.4);
     }
-    table tbody tr.europe td:first-child{
+
+    table tbody tr.europe td:first-child {
         background-color: rgba(25, 88, 0, 0.8) !important;
     }
-    table tbody tr.top td:first-child{
+
+    table tbody tr.top td:first-child {
         background-color: rgba(214, 200, 3, 0.8);
     }
-    table tbody tr.ldcable td:first-child{
+
+    table tbody tr.ldcable td:first-child {
         background-color: rgba(0, 0, 210, 0.4);
     }
-    table tbody tr.ldc td:first-child{
+
+    table tbody tr.ldc td:first-child {
         background-color: rgba(0, 0, 210, 0.8) !important;
     }
 </style>
--- src/routes/ligue2/+page.svelte
+++ src/routes/ligue2/+page.svelte
@@ -1,25 +1,25 @@
 <script>
-    import { onMount } from 'svelte';
+    import {onMount} from 'svelte';
     import axios from 'axios';
-    import { load } from 'cheerio';
-    
+    import {load} from 'cheerio';
+
     const nbMaxJ = 38;
     let ptsRelegation = 0;
     let ptsL1 = 0;
-    
+
     const url = 'https://www.ligue2.fr/classement';
-    
+
     /**
-    * @type {any[]}
-    */
+     * @type {any[]}
+     */
     let clubs = [];
-    
+
     onMount(async function () {
         const response = await axios.get(url)
         const html = response.data;
         const $ = load(html);
-        
-        $('.GeneralStats-item--club .desktop-item').each(function() {
+
+        $('.GeneralStats-item--club .desktop-item').each(function () {
             const name = $(this).text().trim();
             const line = {
                 name: name,
@@ -28,15 +28,14 @@
                 diff: $(this).parent().next().next().next().next().next().next().next().next().text(),
                 maxPts: 0
             };
-            line.maxPts = parseInt(line.pts) + ( 3 * (nbMaxJ - parseInt(line.joues)))
-            clubs = [...clubs, line];            
+            line.maxPts = parseInt(line.pts) + (3 * (nbMaxJ - parseInt(line.joues)))
+            clubs = [...clubs, line];
         });
-        ptsRelegation = parseInt(clubs[16].pts) + ( 3 * (nbMaxJ - parseInt(clubs[16].joues)));
-        ptsL1 = parseInt(clubs[3].pts) + ( 3 * (nbMaxJ - parseInt(clubs[3].joues)));
+        ptsRelegation = parseInt(clubs[16].pts) + (3 * (nbMaxJ - parseInt(clubs[16].joues)));
+        ptsL1 = parseInt(clubs[3].pts) + (3 * (nbMaxJ - parseInt(clubs[3].joues)));
     });
-    
-    
-    
+
+
 </script>
 
 <div class="textcenter">
@@ -44,80 +43,95 @@
 </div>
 <table cellpadding="13" cellspacing="0">
     <thead>
-        <tr>
-            <td>#</td>
-            <td>Name</td>
-            <td>Pts</td>
-            <td>GA</td>
-            <td>J</td>
-            <td>Max</td>
-        </tr>
+    <tr>
+        <td>#</td>
+        <td>Name</td>
+        <td>Pts</td>
+        <td>GA</td>
+        <td>J</td>
+        <td>Max</td>
+        <td>Est.</td>
+
+    </tr>
     </thead>
     <tbody>
-        {#each clubs as club, index}
+    {#each clubs as club, index}
         <tr class='
         {club.pts <= clubs[16].maxPts ? "releguable " : ""} 
         {club.maxPts < clubs[15].pts ? "relegue " : ""} 
         {club.pts > ptsL1 ? "europe " : ""}
         {club.maxPts >= clubs[1].pts ? "europable " : ""} 
         '>
-        
-        <td>
-            {index + 1 }
-        </td>
-        <td>
-            {club.name}
-        </td>
-        <td>
-            {club.pts}
-        </td>
-        <td>{club.diff}</td>
-        <td>{club.joues}</td>
-        <td>{club.maxPts}</td>
-    </tr> 
+
+            <td>
+                {index + 1 }
+            </td>
+            <td>
+                {club.name}
+            </td>
+            <td>
+                {club.pts}
+            </td>
+            <td>{club.diff}</td>
+            <td>{club.joues}</td>
+            <td>{club.maxPts}</td>
+            <td>{Math.round(club.pts + ((club.pts / club.joues) * (nbMaxJ - club.joues)))}</td>
+
+        </tr>
     {/each}
-</tbody>
+    </tbody>
 </table>
 
 
 <style>
-    .textcenter{
+    .textcenter {
         text-align: center;
     }
-    thead td{
+
+    thead td {
         font-weight: bold;
     }
-    table{
+
+    table {
         width: 100%;
         height: 100vh;
     }
-    tbody tr:nth-child(odd){
+
+    tbody tr:nth-child(odd) {
         background: #eee;
     }
+
     tbody tr:nth-last-child(3),
     tbody tr:nth-last-child(2),
-    tbody tr:nth-last-child(1){
+    tbody tr:nth-last-child(1) {
         background-color: rgb(255, 187, 187);
     }
-    table tbody tr.releguable td:first-child{
-        background-color: rgba(255,0,0,0.2);
+
+    table tbody tr.releguable td:first-child {
+        background-color: rgba(255, 0, 0, 0.2);
     }
-    table tbody tr.relegue td:first-child{
-        background-color: red; 
+
+    table tbody tr.relegue td:first-child {
+        background-color: red;
     }
-    table tbody tr.europable td:first-child{
+
+    table tbody tr.europable td:first-child {
         background-color: rgba(25, 88, 0, 0.4);
     }
-    table tbody tr.europe td:first-child{
+
+    table tbody tr.europe td:first-child {
         background-color: rgba(25, 88, 0, 0.8) !important;
     }
-    table tbody tr.top td:first-child{
+
+    table tbody tr.top td:first-child {
         background-color: rgba(214, 200, 3, 0.8);
     }
-    table tbody tr.ldcable td:first-child{
+
+    table tbody tr.ldcable td:first-child {
         background-color: rgba(0, 0, 210, 0.4);
     }
-    table tbody tr.ldc td:first-child{
+
+    table tbody tr.ldc td:first-child {
         background-color: rgba(0, 0, 210, 0.8) !important;
     }
 </style>

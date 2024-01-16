233b14d4a53f253ce7cbbf4444b4d428b8445b22
Sébastien <sgortais@net-com.fr>
2024-01-16 13:44:48 +0100

    New J + range input

--- src/routes/ligue1/+page.svelte
+++ src/routes/ligue1/+page.svelte
@@ -8,95 +8,103 @@
     let ptsEurope = 0;
     let ptsLDC = 0;
     let ptsPremier = 0;  
-    
-    
-    function doMatch(match){
-        if (match.homeScore === null) return false;
-        if(clubs[match.home] === undefined){
-            alert(match.home);
-        }
-        if(clubs[match.ext] === undefined){
-            alert(match.ext);
-        }
-        clubs[match.home].joues += 1;
-        clubs[match.ext].joues += 1;
-        
-        if (match.extScore > match.homeScore){
-            clubs[match.ext].pts += 3;
-            clubs[match.ext].results.push('V');
-            clubs[match.home].results.push('D');
-            let diff = match.extScore  - match.homeScore;
-            clubs[match.ext].diff += diff;
-            clubs[match.home].diff -= diff;
-        } else if (match.extScore === match.homeScore){
-            clubs[match.ext].pts += 1;
-            clubs[match.home].pts += 1;
-            clubs[match.ext].results.push('N');
-            clubs[match.home].results.push('N');
-        } else{
-            clubs[match.home].pts += 3;
-            clubs[match.home].results.push('V');
-            clubs[match.ext].results.push('D');
-            let diff = match.homeScore - match.extScore;
-            clubs[match.ext].diff -= diff;
-            clubs[match.home].diff += diff;
-        };
-        
-        console.log(clubs[match.ext].results.length)
-        if(clubs[match.ext].results.length > 5){
-            clubs[match.ext].results.shift();
-        }
-        if(clubs[match.home].results.length > 5){
-            clubs[match.home].results.shift();
-        }
-    }
-    
+    let splitter = 33;
     let classement = [];
+    let allMatches = data.matches.slice(0, splitter);
     
-    let clubs = {
-        "OGC NICE": {name: 'OGC NICE', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
-        "PARIS SAINT-GERMAIN": {name: 'PARIS SAINT-GERMAIN', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
-        "AS MONACO": {name: 'AS MONACO', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
-        "LOSC LILLE": {name: 'LOSC LILLE', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
-        "STADE BRESTOIS 29": {name: 'STADE BRESTOIS 29', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
-        "FC NANTES": {name: 'FC NANTES', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
-        "STADE DE REIMS": {name: 'STADE DE REIMS', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
-        "OLYMPIQUE DE MARSEILLE": {name: 'OLYMPIQUE DE MARSEILLE', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
-        "STADE RENNAIS FC": {name: 'STADE RENNAIS FC', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
-        "TOULOUSE FC": {name: 'TOULOUSE FC', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
-        "HAVRE ATHLETIC CLUB": {name: 'HAVRE ATHLETIC CLUB', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
-        "FC LORIENT": {name: 'FC LORIENT', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
-        "RC STRASBOURG ALSACE": {name: 'RC STRASBOURG ALSACE', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
-        "MONTPELLIER HÉRAULT SC": {name: 'MONTPELLIER HÉRAULT SC', pts: -1, joues: 0, diff: 0, maxPts: 0,results: []},
-        "RC LENS": {name: 'RC LENS', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
-        "FC METZ": {name: 'FC METZ', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
-        "OLYMPIQUE LYONNAIS": {name: 'OLYMPIQUE LYONNAIS', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
-        "CLERMONT FOOT 63": {name: 'CLERMONT FOOT 63', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []}
-    };
-        
-    data.matches.forEach((journee) => {
-        journee.forEach(match => {
-            doMatch(match);
-        });
-    });
+    $: if (splitter >= 0){
+        console.log('aze')
+        allMatches = data.matches.slice(0, splitter + 1);
     
-    classement = Object.values(clubs); 
-    
-    classement.sort((a, b) => {return b.pts - a.pts || b.diff - a.diff });
-    
-    classement.forEach(team => {
-        team.maxPts = team.pts + (3*(nbMaxJ - team.joues));
-        team.sgs = {
-            v5: team.results.filter(x => x === "V").length,
-            n5: team.results.filter(x => x === "N").length,
+
+        function doMatch(match){
+            if (match.homeScore === null) return false;
+            if(clubs[match.home] === undefined){
+                alert(match.home);
+            }
+            if(clubs[match.ext] === undefined){
+                alert(match.ext);
+            }
+            clubs[match.home].joues += 1;
+            clubs[match.ext].joues += 1;
+            
+            if (match.extScore > match.homeScore){
+                clubs[match.ext].pts += 3;
+                clubs[match.ext].results.push('V');
+                clubs[match.home].results.push('D');
+                let diff = match.extScore  - match.homeScore;
+                clubs[match.ext].diff += diff;
+                clubs[match.home].diff -= diff;
+            } else if (match.extScore === match.homeScore){
+                clubs[match.ext].pts += 1;
+                clubs[match.home].pts += 1;
+                clubs[match.ext].results.push('N');
+                clubs[match.home].results.push('N');
+            } else{
+                clubs[match.home].pts += 3;
+                clubs[match.home].results.push('V');
+                clubs[match.ext].results.push('D');
+                let diff = match.homeScore - match.extScore;
+                clubs[match.ext].diff -= diff;
+                clubs[match.home].diff += diff;
+            };
+            
+            if(clubs[match.ext].results.length > 5){
+                clubs[match.ext].results.shift();
+            }
+            if(clubs[match.home].results.length > 5){
+                clubs[match.home].results.shift();
+            }
         }
-    })
-    ptsRelegation = classement[16].pts + (3 * (nbMaxJ - classement[16].joues));
-    ptsEurope = classement[5].pts + (3 * (nbMaxJ - classement[5].joues));        
-    ptsLDC = classement[4].pts + (3 * (nbMaxJ - classement[4].joues));
-    ptsPremier = classement[1].pts + (3 * (nbMaxJ - classement[1].joues));
+        
+        
+        let clubs = {
+            "OGC NICE": {name: 'OGC NICE', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
+            "PARIS SAINT-GERMAIN": {name: 'PARIS SAINT-GERMAIN', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
+            "AS MONACO": {name: 'AS MONACO', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
+            "LOSC LILLE": {name: 'LOSC LILLE', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
+            "STADE BRESTOIS 29": {name: 'STADE BRESTOIS 29', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
+            "FC NANTES": {name: 'FC NANTES', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
+            "STADE DE REIMS": {name: 'STADE DE REIMS', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
+            "OLYMPIQUE DE MARSEILLE": {name: 'OLYMPIQUE DE MARSEILLE', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
+            "STADE RENNAIS FC": {name: 'STADE RENNAIS FC', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
+            "TOULOUSE FC": {name: 'TOULOUSE FC', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
+            "HAVRE ATHLETIC CLUB": {name: 'HAVRE ATHLETIC CLUB', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
+            "FC LORIENT": {name: 'FC LORIENT', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
+            "RC STRASBOURG ALSACE": {name: 'RC STRASBOURG ALSACE', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
+            "MONTPELLIER HÉRAULT SC": {name: 'MONTPELLIER HÉRAULT SC', pts: -1, joues: 0, diff: 0, maxPts: 0,results: []},
+            "RC LENS": {name: 'RC LENS', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
+            "FC METZ": {name: 'FC METZ', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
+            "OLYMPIQUE LYONNAIS": {name: 'OLYMPIQUE LYONNAIS', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []},
+            "CLERMONT FOOT 63": {name: 'CLERMONT FOOT 63', pts: 0, joues: 0, diff: 0, maxPts: 0,results: []}
+        };
+            
+        allMatches.forEach((journee) => {
+            journee.forEach(match => {
+                doMatch(match);
+            });
+        });
+
+
+        classement = Object.values(clubs); 
+        
+        classement.sort((a, b) => {return b.pts - a.pts || b.diff - a.diff });
+        
+        classement.forEach(team => {
+            team.maxPts = team.pts + (3*(nbMaxJ - team.joues));
+            team.sgs = {
+                v5: team.results.filter(x => x === "V").length,
+                n5: team.results.filter(x => x === "N").length,
+            }
+        })
+        ptsRelegation = classement[16].pts + (3 * (nbMaxJ - classement[16].joues));
+        ptsEurope = classement[5].pts + (3 * (nbMaxJ - classement[5].joues));        
+        ptsLDC = classement[4].pts + (3 * (nbMaxJ - classement[4].joues));
+        ptsPremier = classement[1].pts + (3 * (nbMaxJ - classement[1].joues));
+    }
 </script>
-<Menu />
+
+<input type=range min=0 max={nbMaxJ - 1} bind:value={splitter} /> {splitter + 1}
 <table cellpadding="13" cellspacing="0">
     <thead>
         <tr>

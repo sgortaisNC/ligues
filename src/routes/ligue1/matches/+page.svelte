395c0adb5540215abff84df31057528bdd3097f6
Sébastien <sgortais@net-com.fr>
2024-03-18 11:53:48 +0100

    J26

--- src/routes/ligue1/matches/+page.svelte
+++ src/routes/ligue1/matches/+page.svelte
@@ -2,17 +2,17 @@
     import matches from "../l1.json";
     const journees = matches.matches; 
     let restant = [];
+    let currentJournee= 27;
     journees.forEach((journee,index) => {
-
-        journee.forEach((match,mIndex) => {
-            if(match.homeScore === null || match.extScore === null){
+        if (index === currentJournee - 1 ){
+            journee.forEach((match,mIndex) => {
                 restant.push({
                     journee: index,
                     matchIndex: mIndex,
                     match: match
                 });
-            }
-        });
+            });
+        }
     });
 
     async function sendHome(e){
@@ -41,10 +41,12 @@
 <div class="grid">
 <h1>Liste</h1>
 {#each restant as match}
-    <p>{match.journee}.{match.matchIndex} <br> {match.match.home} 
+    <p>{match.match.home}
+        <span> 
         <input type="number" data-j={match.journee} data-i={match.matchIndex} value={match.match.homeScore} on:change={sendHome}>
         -
         <input type="number" data-j={match.journee} data-i={match.matchIndex} value={match.match.extScore} on:change={sendAway}>
+        </span>
         {match.match.ext}
     </p>
 {/each}
@@ -59,6 +61,10 @@
     p{
         width: 100vw;
         text-align: center;
+        display: grid;
+        grid-template-columns: repeat(3,1fr);
+        align-items: center;
+        font-size: 14px;
     }
     input{
         width: 75px;
